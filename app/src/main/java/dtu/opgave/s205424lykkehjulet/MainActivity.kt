50daaef0ff82af4f70e5fc518efeb29ddd2f9c2d
louismonty <louis.mail.dk@gmail.com>
2021-11-23 19:39:28 +0100

    a lot

--- app/src/main/java/dtu/opgave/s205424lykkehjulet/MainActivity.kt
+++ app/src/main/java/dtu/opgave/s205424lykkehjulet/MainActivity.kt
@@ -1,20 +1,41 @@
 package dtu.opgave.s205424lykkehjulet
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.navigation.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import dtu.opgave.s205424lykkehjulet.R
+import dtu.opgave.s205424lykkehjulet.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment_container)
 
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Get the navigation controller for the navigation host
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the bottom navigation for use with a navigation controller
+        binding.bottomNavView.setupWithNavController(navController)
+
+        // Setup the Action bar so the title changes to the destination
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_favourite, R.id.navigation_recent
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
 }
